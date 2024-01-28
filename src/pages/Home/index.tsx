1e64be2da748b6beab564026acfc177ab6d1b3fb
TamirlanShakh <t.shakhbanov@yandex.ru>
2024-01-28 22:33:03 +0300

    fix element

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../../includes/Header/index.tsx';
 import Footer from '../../includes/Footer/index.tsx';
 import SimpleSlider from '../../slider/Slider.tsx';
+import Phenomenon from '../../includes/Phenomenon/index.tsx';
 
 const Home = () => {
     return (
         <>
             <Header />
-            <SimpleSlider />
+            <Phenomenon />
+            {/* <SimpleSlider /> */}
             <Footer />
         </>
     );
