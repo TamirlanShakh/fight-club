0a92d36f9984dd9a23d1e25f771246c0494823f7
Alexandr <a.s.navtsenya@gmail.com>
2024-02-27 18:31:05 +0300

    Contacts

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './About.scss';
 
 const About = () => {
-    return <div className="about">xnnsdkvdsflkm</div>;
+    return (
+        <div className="about">
+            <div className="agreement">Нажимая "Отправить" вы даёте согласие на обработку ваших персональных данных</div>
+
+            <div className="contact-nav">
+                <div className="map-navigator"></div>
+                <div className="contact-container">
+                    <div className="contact-container__address">г. Москва, ул. Первая 50 к1</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default About;
