1bc80518f467a624a0200849d7693ba337d4d187
TamirlanShakh <t.shakhbanov@yandex.ru>
2024-03-24 18:16:47 +0300

    footer

--- src/includes/Footer/index.tsx
+++ src/includes/Footer/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <>
-      <footer className="bg-gray-800 py-6">Подвал</footer>
-    </>
-  );
+    return (
+        <>
+            <footer className="footer">
+                <div className="container">
+                    <img src="/images/logo.png" className="footer__logo" alt="" />
+                    <div className="footer__info">
+                        <span>© Frota Academy, ООО «Барса» ИНН 9703089591</span>
+                        <span>Все права защищены. Полное или частичное копирование информации с сайта запрещено.</span>
+                        <span>Данный сайт носит исключительно информационный характер и ни при каких условиях не является публичной офертой.</span>
+                    </div>
+                    <Link to="" className="privacy">
+                        Политика в отношении обработки персональных данных
+                    </Link>
+                </div>
+            </footer>
+        </>
+    );
 };
 
 export default Footer;
