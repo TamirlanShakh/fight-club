fd4d306a93a1154f39c18be07c211b59340f0d3c
TamirlanShakh <t.shakhbanov@yandex.ru>
2024-03-31 19:35:05 +0300

    fix

--- src/includes/Footer/index.tsx
+++ src/includes/Footer/index.tsx
@@ -9,11 +9,7 @@ const Footer = () => {
                 <div className="container">
                     <img src="/images/logo.png" className="footer__logo" alt="" />
                     <div className="footer__info">
-<<<<<<< HEAD
                         <span>© Fight Club, ООО «Файт Клаб» ИНН 0000000000</span>
-=======
-                        <span>© Fight Club, ООО «Файт Клаб» ИНН 0000000001</span>
->>>>>>> reviews
                         <span>Все права защищены. Полное или частичное копирование информации с сайта запрещено.</span>
                         <span>Данный сайт носит исключительно информационный характер и ни при каких условиях не является публичной офертой.</span>
                     </div>
