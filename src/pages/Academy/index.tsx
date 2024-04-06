7869741b0c61a2c62573f432d46d7f704b2d6b6b
TamirlanShakh <t.shakhbanov@yandex.ru>
2024-04-06 15:33:31 +0300

    fonts

---
+++ src/pages/Academy/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Academy = () => {
+    return (
+        <>
+            <section className="academy">
+                <div className="container"></div>
+            </section>
+        </>
+    );
+};
+
+export default Academy;
